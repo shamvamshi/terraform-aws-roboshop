ea92966db18b2b529f4734bb774d36d351a8173b
shamvamshi <shamvamshiwork@gmail.com>
2025-09-09 17:14:14 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0148010RVYSUD4B0YSR"
+}
+
+variable "zone_name" {
+  default = "shamrobos.site"
+}
+
+variable "component" {
+  
+}
+
+variable "rule_priority" {
+  
+}
